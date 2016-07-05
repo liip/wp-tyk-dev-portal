83a906d431c56527f8d9eb026db125832a75d5c4
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-07-05 12:05:08 +0200

    added separate file for template tags

---
+++ template_tags.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Get the form tag for the api subscription form
+ * 
+ * @param string $class An optional class to add to the form tag
+ * @return string
+ */
+function tyk_dev_portal_api_subscribe_form($class = '') {
+	return printf('<form action="%s" class="%s" method="post">
+		<input type="hidden" name="action" value="get_token">',
+		esc_url(admin_url('admin-post.php')),
+		$class
+		);
+}
+
+/**
+ * Get <select> pulldown for api subscription
+ *
+ * @param string $class An optional class to add to the select tag
+ * @return string
+ */
+function tyk_dev_portal_api_select($class = '') {
+	$opts = array();
+	foreach (Tyk_API_Manager::available_apis() as $api) {
+		$opts[] = sprintf('<option value="%s">%s</option>', 
+			$api->id, 
+			$api->name
+			);
+	}
+	return printf('<select name="api" id="api-select" class="%s">%s</select>',
+		$class,
+		join("\n", $opts)
+		);
+}
