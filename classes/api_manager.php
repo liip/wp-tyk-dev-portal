94d03b3d8baa987cd2e9690fc1cda8278432210b
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-07-05 12:05:22 +0200

    added Tyk_API_Manager class

---
+++ classes/api_manager.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Tyk API manager
+ *
+ * @package Tyk_Dev_Portal\Tyk_API
+ */
+
+/**
+ * Handles API management, not actual API communication
+ */
+class Tyk_API_Manager 
+{
+	/**
+	 * Get available APIs defined on Tyk
+	 * 
+	 * @return array
+	 */
+	public static function available_apis() {
+		$tyk = new Tyk_API();
+		$response = $tyk->get('/apis');
+		// a generator would be nice here but alas, php 5.4 is still very common
+		$active_apis = array();
+		foreach ($response->apis as $api) {
+			// only return active apis
+			if ($api->api_definition->active) {
+				$active_apis[] = $api->api_definition;
+			}
+		}
+		return $active_apis;
+	}
+
+	/**
+	 * Register a developer for a tyk API
+	 * 
+	 * @param Portal_User $user
+	 * @param string $policy
+	 * @return [type]
+	 */
+	public function registerForAPI(Tyk_Portal_User $user, $policy) {
+		try {
+			$tyk = new Tyk_API();
+			$key = $tyk->post('/portal/requests', array(
+				'by_user' => $user->get_tyk_id(),
+				'for_plan' => $policy,
+				// it's possible to have key requests approved manually
+				'approved' => TYK_AUTO_APPROVE_KEY_REQUESTS,
+				// this is a bit absurd
+				'date_created' => date('c')
+				));
+			// @todo show key
+		}
+		catch (Exception $e) {
+			// @todo show error in form?
+		}
+	}
+}
