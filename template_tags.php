82d243a85fc29e294b1ec5c58df64d155c18275c
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-09-19 16:38:58 +0200

    added a message that you must be logged-in to view the dashboard and translated it

--- template_tags.php
+++ template_tags.php
@@ -11,4 +11,8 @@ function tyk_dev_portal_dashboard() {
 	if ($user->is_logged_in()) {
 		include_once TYK_DEV_PORTAL_PLUGIN_PATH . '/templates/api_subscribe_form.php';
 	}
+	else {
+		$login_page = get_page_by_path('log-in');
+		printf(__('This page is only available when you are <a href="%1$s">logged in</a>.', Tyk_Dev_Portal::TEXT_DOMAIN), get_permalink($login_page));
+	}
 }
