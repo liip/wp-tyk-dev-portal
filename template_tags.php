ac4bdfd9f30ad03188d866043dfe461025c25174
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-11-07 13:15:30 +0100

    return developer dashboard content instead of printing it

--- template_tags.php
+++ template_tags.php
@@ -2,17 +2,19 @@
 
 /**
  * Get the developer dashboard page
- * Prints directly to output
+ * Returns the html code or an error message when user is not logged in
  * 
- * @return void
+ * @return string
  */
 function tyk_dev_portal_dashboard() {
 	$user = new Tyk_Portal_User;
 	if ($user->is_logged_in()) {
+		ob_start();
 		include_once TYK_DEV_PORTAL_PLUGIN_PATH . '/templates/api_subscribe_form.php';
+		return ob_get_clean();
 	}
 	else {
 		$login_page = get_page_by_path('log-in');
-		printf(__('This page is only available when you are <a href="%1$s">logged in</a>.', Tyk_Dev_Portal::TEXT_DOMAIN), get_permalink($login_page));
+		return sprintf(__('This page is only available when you are <a href="%1$s">logged in</a>.', Tyk_Dev_Portal::TEXT_DOMAIN), get_permalink($login_page));
 	}
 }
