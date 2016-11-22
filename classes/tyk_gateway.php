4a1ab60b5730b3dac4801b4e3b3c0921f7118309
Christof Leuenberger <christof.leuenberger@liip.ch>
2016-11-22 09:14:05 +0100

    converted indentation to tabs

--- classes/tyk_gateway.php
+++ classes/tyk_gateway.php
@@ -11,32 +11,32 @@
  */
 class Tyk_Gateway extends Tyk_Interaction
 {
-    /**
-     * Send a get request to Tyk gateway
-     * 
-     * @param string $path
-     * @param array $args  Query string args
-     *
-     * @throws Exception When API sends invalid response
-     * 
-     * @return array
-     */
-    public function get($path, array $args = null) {
-    	// if gateway credentials are configured, setup 
-        $api_response = wp_remote_get($this->get_url_for_path($path, $args), array(
-            'headers' => array(
-                'x-tyk-authorization' => TYK_GATEWAY_SECRET
-            ),
-        ));
+	/**
+	 * Send a get request to Tyk gateway
+	 * 
+	 * @param string $path
+	 * @param array $args  Query string args
+	 *
+	 * @throws Exception When API sends invalid response
+	 * 
+	 * @return array
+	 */
+	public function get($path, array $args = null) {
+		// if gateway credentials are configured, setup 
+		$api_response = wp_remote_get($this->get_url_for_path($path, $args), array(
+			'headers' => array(
+				'x-tyk-authorization' => TYK_GATEWAY_SECRET
+			),
+		));
 
-        $response = $this->parse_response($api_response);
-        if (is_object($response)) {
-            return $response;
-        }
-        else {
-            throw new Exception('Received invalid response from Gateway');
-        }
-    }
+		$response = $this->parse_response($api_response);
+		if (is_object($response)) {
+			return $response;
+		}
+		else {
+			throw new Exception('Received invalid response from Gateway');
+		}
+	}
 
 	/**
 	 * Send a delete request to Tyk gateway
@@ -52,7 +52,7 @@ class Tyk_Gateway extends Tyk_Interaction
 		$api_response = wp_remote_request($url, array(
 			'method' => 'DELETE',
 			'headers' => array(
-                'x-tyk-authorization' => TYK_GATEWAY_SECRET
+				'x-tyk-authorization' => TYK_GATEWAY_SECRET
 			)));
 
 		$response = $this->parse_response($api_response);
@@ -62,12 +62,12 @@ class Tyk_Gateway extends Tyk_Interaction
 		else {
 			throw new Exception('Received invalid response from API');
 		}
-    }
+	}
 
 	/**
 	 * Get absolute url to api endpoint for a path
 	 * 
-     * @param string $path
+	 * @param string $path
 	 * @return string
 	 */
 	protected function get_url_for_path($path, array $args = null) {
@@ -75,7 +75,7 @@ class Tyk_Gateway extends Tyk_Interaction
 		$qs = '';
 		if (is_array($args)) {
 			$qs = '?' . http_build_query($args);
-        }
+		}
 		return sprintf('%s/%s%s', 
 			rtrim(TYK_GATEWAY_URL, '/'), 
 			ltrim($path, '/'),
